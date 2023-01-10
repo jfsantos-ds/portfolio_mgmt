a06e87a76b24e277adc77828c287b1c6c6054144
Francisco Santos <36741643+jfsantos-ds@users.noreply.github.com>
2023-01-10 19:08:49 +0000

    remove old src dir (#6)

--- portfolio_mgmt/client/client.py
+++ portfolio_mgmt/client/client.py
@@ -4,9 +4,9 @@ Client for the Portfolio Manager.
 from datetime import datetime as dt, timedelta as td
 from getpass import getpass
 from typing import Optional, Union
-import requests
 
-from degiroapi import DeGiro, ClientInfo, datatypes
+import requests
+from degiroapi import ClientInfo, DeGiro, datatypes
 from portfolio import Portfolio
 from portfolio_item import PortfolioItem
 
@@ -26,12 +26,21 @@ class Client(DeGiro):
 
         self.login(keep_pass)
 
-    def __request(self, url, cookie=None, payload=None, headers={}, data=None, post_params=None, request_type=None,
-                  error_message='An error occurred.'):
+    def __request(
+        self,
+        url,
+        cookie=None,
+        payload=None,
+        headers={},
+        data=None,
+        post_params=None,
+        request_type=None,
+        error_message="An error occurred.",
+    ):
         if not request_type:
             request_type = self._DeGiro__GET_REQUEST
 
-        headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0'
+        headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0"
 
         if request_type == self._DeGiro__DELETE_REQUEST:
             response = requests.delete(url, json=payload)
@@ -46,7 +55,7 @@ class Client(DeGiro):
         elif request_type == self._DeGiro__POST_REQUEST:
             response = requests.post(url, headers=headers, json=payload)
         else:
-            raise Exception(f'Unknown request type: {request_type}')
+            raise Exception(f"Unknown request type: {request_type}")
 
         if response.status_code == 200 or response.status_code == 201:
             try:
@@ -54,31 +63,39 @@ class Client(DeGiro):
             except:
                 return "No data"
         else:
-            raise Exception(f'{error_message} Response: {response.text}')
-            
+            raise Exception(f"{error_message} Response: {response.text}")
+
     def __auth(self, username, password, totp):
         login_payload = {
-            'username': username,
-            'password': password,
-            'isPassCodeReset': False,
-            'isRedirectToMobile': False,
-            'oneTimePassword': totp
+            "username": username,
+            "password": password,
+            "isPassCodeReset": False,
+            "isRedirectToMobile": False,
+            "oneTimePassword": totp,
         }
-        login_response = self.__request(self.__LOGIN_URL, None, login_payload, request_type=self._DeGiro__POST_REQUEST,
-                                        error_message='Could not login.')
-        self.session_id = login_response['sessionId']
-        client_info_payload = {'sessionId': self.session_id}
-        client_info_response = self.__request(self._DeGiro__CLIENT_INFO_URL, None, client_info_payload,
-                                              error_message='Could not get client info.')
-        self.client_info = ClientInfo(client_info_response['data'])
+        login_response = self.__request(
+            self.__LOGIN_URL,
+            None,
+            login_payload,
+            request_type=self._DeGiro__POST_REQUEST,
+            error_message="Could not login.",
+        )
+        self.session_id = login_response["sessionId"]
+        client_info_payload = {"sessionId": self.session_id}
+        client_info_response = self.__request(
+            self._DeGiro__CLIENT_INFO_URL, None, client_info_payload, error_message="Could not get client info."
+        )
+        self.client_info = ClientInfo(client_info_response["data"])
 
-        cookie = {
-            'JSESSIONID': self.session_id
-        }
+        cookie = {"JSESSIONID": self.session_id}
 
-        client_token_response = self.__request(self._DeGiro__CONFIG_URL, cookie=cookie, request_type=self._DeGiro__GET_REQUEST,
-                                               error_message='Could not get client config.')
-        self.client_token = client_token_response['data']['clientId']
+        client_token_response = self.__request(
+            self._DeGiro__CONFIG_URL,
+            cookie=cookie,
+            request_type=self._DeGiro__GET_REQUEST,
+            error_message="Could not get client config.",
+        )
+        self.client_token = client_token_response["data"]["clientId"]
 
         return client_info_response
 
@@ -113,27 +130,34 @@ class Client(DeGiro):
         return self._portfolio
 
     def getdata(self, datatype, filter_zero=None):
-        data_payload = {
-            datatype: 0
-        }
+        data_payload = {datatype: 0}
 
         if datatype == datatypes.Data.Type.CASHFUNDS:
             return self.filtercashfunds(
-                self.__request(self._DeGiro__DATA_URL + str(self.client_info.account_id) + ';jsessionid=' + self.session_id,
-                               None,
-                               data_payload,
-                               error_message='Could not get data'))
+                self.__request(
+                    self._DeGiro__DATA_URL + str(self.client_info.account_id) + ";jsessionid=" + self.session_id,
+                    None,
+                    data_payload,
+                    error_message="Could not get data",
+                )
+            )
         elif datatype == datatypes.Data.Type.PORTFOLIO:
             return self.filterportfolio(
-                self.__request(self._DeGiro__DATA_URL + str(self.client_info.account_id) + ';jsessionid=' + self.session_id,
-                               None,
-                               data_payload,
-                               error_message='Could not get data'), filter_zero)
+                self.__request(
+                    self._DeGiro__DATA_URL + str(self.client_info.account_id) + ";jsessionid=" + self.session_id,
+                    None,
+                    data_payload,
+                    error_message="Could not get data",
+                ),
+                filter_zero,
+            )
         else:
             return self.__request(
-                self._DeGiro__DATA_URL + str(self.client_info.account_id) + ';jsessionid=' + self.session_id, None,
+                self._DeGiro__DATA_URL + str(self.client_info.account_id) + ";jsessionid=" + self.session_id,
+                None,
                 data_payload,
-                error_message='Could not get data')
+                error_message="Could not get data",
+            )
 
     def get_balance(self, _print: bool = True) -> dict:
         "Returns cash funds of the account. Prints items by default, disable with '_print=False'."
@@ -187,14 +211,15 @@ class Client(DeGiro):
 
     def _transactions(self, from_date, to_date, group_transactions=False):
         transactions_payload = {
-            'fromDate': from_date.strftime('%d/%m/%Y'),
-            'toDate': to_date.strftime('%d/%m/%Y'),
-            'group_transactions_by_order': group_transactions,
-            'intAccount': self.client_info.account_id,
-            'sessionId': self.session_id
+            "fromDate": from_date.strftime("%d/%m/%Y"),
+            "toDate": to_date.strftime("%d/%m/%Y"),
+            "group_transactions_by_order": group_transactions,
+            "intAccount": self.client_info.account_id,
+            "sessionId": self.session_id,
         }
-        return self.__request(self._DeGiro__TRANSACTIONS_URL, None, transactions_payload,
-                              error_message='Could not get transactions.')['data']
+        return self.__request(
+            self._DeGiro__TRANSACTIONS_URL, None, transactions_payload, error_message="Could not get transactions."
+        )["data"]
 
 
 if __name__ == "__main__":
