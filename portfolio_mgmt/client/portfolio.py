aa7dcafd06034b4930ca9480796fb220eac79f46
Francisco Santos <36741643+jfsantos-ds@users.noreply.github.com>
2023-01-12 16:54:41 +0000

    degiro product update (#7)

--- portfolio_mgmt/client/portfolio.py
+++ portfolio_mgmt/client/portfolio.py
@@ -7,24 +7,12 @@ from colorama import Fore
 from product import Product
 
 
-class Portfolio:
+class Portfolio(list):
     def __init__(self, items: Optional[List[Product]], client=None):
-        self.items = None
+        super().__init__(item for item in items)
+        self.client = client
         self.cash = None
-        self.start = None
-        self.end = None
-        self.history = None
         self.last_updated = None
-        if items:
-            self.set_portfolio(self, client)
-
-    def set_portfolio(self, client: List[Product]):
-        raise NotImplementedError
-
-    @staticmethod
-    def load_portfolio(file_name: str, decryption_key: str):
-        "Decr"
-        raise NotImplementedError
 
     def get_history(self, cache_prefix: str = "../../history/", store: bool = True, update: bool = True) -> dict:
         """Checks for the account history of the client id in a cache directory. Reads and returns in case it exists.
@@ -47,10 +35,3 @@ class Portfolio:
         self.wavg_price = portfolio_listing.get("breakEvenPrice")  # Degiro calls it Break Even Price
         self.value = portfolio_listing.get("value")
         self.last_updated = None
-
-    def describe(self):
-        if self.type == "currency":
-            return f"{self.size} {self.currency}"
-        returns = self.price / self.wavg_price - 1
-        returns_substring = (Fore.GREEN + "+" if returns >= 0 else Fore.RED) + f"{returns:.1%}" + Fore.RESET
-        return f"{self.size} {self.name[:30]} @ {self.price:.2f}{self.currency} / {self.wavg_price:.2f}{self.currency} {returns_substring}"
--- portfolio_mgmt/client/portfolio_item.py
+++ portfolio_mgmt/client/portfolio_item.py
@@ -8,12 +8,15 @@ from product import Product
 
 
 class PortfolioItem(Product):
+    PRINT_LEN = 30
+
     def __init__(self, portfolio_listing: Optional[dict], client=None):
         self.size = None
         self.price = None
         self.wavg_price = None
         self.value = None
         self.last_updated = None
+        self.purchased = False
         if portfolio_listing:
             id = portfolio_listing["id"]
             super().__init__(id, client)
@@ -24,12 +27,16 @@ class PortfolioItem(Product):
         self.size = portfolio_listing.get("size")
         self.price = portfolio_listing.get("price")
         self.wavg_price = portfolio_listing.get("breakEvenPrice")  # Degiro calls it Break Even Price
+        self.purchased = self.wavg_price == 0
         self.value = portfolio_listing.get("value")
         self.last_updated = None
 
     def __str__(self):
         if self.type == "currency":
             return f"{self.size} {self.currency}"
-        returns = self.price / self.wavg_price - 1
-        returns_substring = (Fore.GREEN + "+" if returns >= 0 else Fore.RED) + f"{returns:.1%}" + Fore.RESET
-        return f"{self.size} {self.name[:30]} @ {self.price:.2f}{self.currency} / {self.wavg_price:.2f}{self.currency} {returns_substring}"
+        if self.purchased:
+            returns = self.price*self.size
+        else:
+            returns = self.price/self.wavg_price - 1
+        returns_substring = (Fore.GREEN+"+" if returns>=0 else Fore.RED) + (f"{self.price:.2f}{self.currency}" if self.purchased else f"{returns:.1%}") + Fore.RESET
+        return f"{self.size} {self.name[:self.PRINT_LEN]} @ {self.wavg_price:.2f}{self.currency} / {self.price:.2f}{self.currency} {returns_substring}"
